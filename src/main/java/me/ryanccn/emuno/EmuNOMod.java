66a0bbf7d0a15f48fad44d360b76c786eaa29597
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-01 23:14:25 +0800

    initial commit

---
+++ src/main/java/me/ryanccn/emuno/EmuNOMod.java
@@ -0,0 +1,14 @@
+package me.ryanccn.emuno;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmuNOMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("EmuNO has initialized! Brrr");
+	}
+}
---
+++ src/main/java/me/ryanccn/emuno/mixin/EmuNOMixin.java
@@ -0,0 +1,16 @@
+package me.ryanccn.emuno.mixin;
+
+import net.minecraft.client.Mouse;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Mouse.class)
+public class EmuNOMixin {
+	@Redirect(method = "onMouseButton", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;IS_SYSTEM_MAC:Z"))
+	private boolean pretendToNotBeMacOS() {
+		// amogus
+		return false;
+	}
+}
