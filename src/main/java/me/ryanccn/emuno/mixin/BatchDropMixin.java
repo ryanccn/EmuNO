a94c750e3c60a2ee4f0b721c401fbb6f13d7e0ef
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-16 16:35:15 +0800

    my bad

---
+++ src/main/java/me/ryanccn/emuno/mixin/BatchDropMixin.java
@@ -0,0 +1,20 @@
+package me.ryanccn.emuno.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MinecraftClient.class)
+public class BatchDropMixin {
+    @Redirect(
+            method = "handleInputEvents()V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;hasControlDown()Z")
+    )
+    private boolean hasActualCtrlDown() {
+        return InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_CONTROL) || InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_CONTROL);
+    }
+}
