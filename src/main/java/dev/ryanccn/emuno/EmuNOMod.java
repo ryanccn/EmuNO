a21e957a05ba310fea3ab446145656e9a9f672e4
Orion Leon <orionzleon@gmail.com>
2023-11-13 16:42:53 -0800

    Update stuff

--- src/main/java/dev/ryanccn/emuno/EmuNOMod.java
+++ src/main/java/dev/ryanccn/emuno/EmuNOMod.java
@@ -1,12 +1,11 @@
 package dev.ryanccn.emuno;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EmuNOMod implements ModInitializer {
+public class EmuNOMod implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("emuno");
-
 	@Override
 	public void onInitialize() {
 		LOGGER.info("EmuNO has initialized! Enjoy gaming :D");
--- src/main/java/dev/ryanccn/emuno/mixin/BatchDropMixin.java
+++ src/main/java/dev/ryanccn/emuno/mixin/BatchDropMixin.java
@@ -3,7 +3,6 @@ package dev.ryanccn.emuno.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
